fed957057dfe4c05af1e13b9163dcde9a08b1a9b
Evan Cheng <evanchengdesign@gmail.com>
2024-04-18 16:31:27 -0400

    add css for landing page

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Button, Container, Row, Col, Image } from 'react-bootstrap';
-import '../css/landingpage.css'; // Make sure this is correctly imported
+import '../css/landingpage.css';
 
 const LandingPage = () => {
   const { loginWithRedirect } = useAuth0();
 
   return (
-    <Container className="landing-page">
-      <Row>
-        <Col xs={12} className="text-center">
-          <Image src="frontend/src/assets/logo.png" alt="Travel Manager Logo"/>
-          <h1>Travel Manager</h1>
-          <p>Please log in to access the app features.</p>
-          <Button variant="primary" onClick={() => loginWithRedirect()}>Log In</Button>
-        </Col>
-      </Row>
-    </Container>
+    <div className="landing-page">
+      <Container className='container'>
+        <Row>
+          <Col xs={12} className="text-center">
+            <Image src="frontend/src/assets/logo.png" alt="Travel Manager Logo" />
+            <h1>Travel Manager</h1>
+            <p>Please log in to access the app features.</p>
+            <Button className='button' variant="warning" size="sm" onClick={() => loginWithRedirect()}>Log In</Button>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
